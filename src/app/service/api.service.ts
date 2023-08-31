bd5ec44113dcd0c9ea24bccf86485befaed86175
luiseduardot17 <luis_eduardogdeo@hotmail.com>
2023-08-31 00:48:37 -0300

    ✨ feat: agora o service faz somente o post

--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -1,33 +1,17 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
-import { catchError, map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 import { IMensagem } from '../interfaces/IMensagem';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ApiService {
-  private apiUrl = 'http://localhost:3000/mensagem';
+  private apiUrl = 'https://apiconvem.onrender.com/mensagem';
 
   constructor(private http: HttpClient) {}
 
-  validate(text: string): Observable<IMensagem[]> {
-    const textLowerCase = text.toLowerCase();
-
-    if (textLowerCase === 'sim') {
-      return this.http.get<IMensagem[]>(this.apiUrl).pipe(
-        map((mensagens: IMensagem[]) => {
-          return mensagens;
-        }),
-        catchError((error) => {
-          const errorMessage = 'Erro ao buscar mensagem: ' + error.message;
-          return throwError(errorMessage);
-        })
-      );
-    } else {
-      const errorMessage = 'Erro: Entrada inválida';
-      return throwError(errorMessage);
-    }
+  sendResponse(response: string): Observable<IMensagem> {
+    return this.http.post<IMensagem>(this.apiUrl, { resposta: response });
   }
 }
